5ddfa31195f695cfa9f170c5ddd4a30205eb1f53
asahina820 <hina.gntm@gmail.com>
2025-02-13 20:36:02 +0900

    add initial code

--- src/pages/4_sunlight/main.ts
+++ src/pages/4_sunlight/main.ts
@@ -1,4 +1,4 @@
-import ThreeView from "@navara/three";
+import ThreeView, { JAPAN_GSI_ELEVATION_DECODER } from "@navara/three";
 
 const view = new ThreeView({});
 await view.init();
@@ -10,5 +10,15 @@ view.addLayer({
   },
   raster_tile: {
     max_zoom: 23,
+  },
+});
+
+view.addLayer({
+  type: "terrain",
+  data: {
+    url: "https://cyberjapandata.gsi.go.jp/xyz/dem_png/{z}/{x}/{y}.png",
+  },
+  raster_terrain: {
+    elevation_decoder: JAPAN_GSI_ELEVATION_DECODER(),
   },
 });
